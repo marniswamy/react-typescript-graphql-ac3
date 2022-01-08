c0b5b7969c99b9a62224ea1c3151c382d7cc9ee5
Marni Ayyappa Swamy <ayyappa.marni@hotmail.com>
2022-01-08 19:39:21 +0100

    Loader component added

--- src/pages/IssuesList/IssuesList.tsx
+++ src/pages/IssuesList/IssuesList.tsx
@@ -1,9 +1,17 @@
 import React from "react";
+import Loader from "../../components/Loader/Loader";
+import "./issuesList.css";
 
 const IssuesList = () => {
   return (
-    <div className="issues-list">
-      <h1>IssuesList</h1>
+    <div className="issues-container">
+      <h1>List of React Repository Issues</h1>
+      <div className="issues-list">
+        <ul>
+          <li>issues 1</li>
+          <li>issues 2</li>
+        </ul>
+      </div>
     </div>
   );
 };
